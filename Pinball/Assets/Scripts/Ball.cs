062c88d33747976dabd1d2d8ea10b67036cd77c7
Nicolas Valdearenas <nicolasvalde@gmail.com>
2018-07-09 21:54:22 -0300

    Agregado proyecto pinball estable

---
+++ Pinball/Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private Rigidbody rbody;
+    public float x = 43;
+    private float y = 1;
+    public float z = 6.93F;
+
+    private GameController gameController;
+
+    // Use this for initialization
+    void Start () {
+        rbody = GetComponent<Rigidbody>();
+
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.name == "Abajo")
+        {
+            print("Perdiste");
+            gameController.restarVida();
+            transform.position = new Vector3(x, y, z);
+        } 
+    }
+
+}
---
+++ Pinball/Assets/Scripts/GameController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Text textoPuntaje;
+    public Text textoVidas;
+
+    public GameObject gameOverData;
+    public Text textoPuntajeGameOver;
+
+    private int puntos;
+    private int vidas;
+
+    // Use this for initialization
+    void Start () {
+        puntos = 0;
+        vidas = 3;
+        actualizarPuntaje();
+        actualizarVidas();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void sumarPuntos(int puntosNuevos)
+    {
+        puntos += puntosNuevos;
+        actualizarPuntaje();
+    }
+
+    public void restarVida()
+    {
+        vidas--;
+        actualizarVidas();
+
+        if (vidas == 0)
+        {
+            gameOver();
+        }
+        
+    }
+
+    void actualizarPuntaje()
+    {
+        textoPuntaje.text = puntos + " puntos";
+    }
+
+    void actualizarVidas()
+    {
+        textoVidas.text = vidas + " vidas";
+    }
+
+    void gameOver()
+    {
+        Debug.Log("PERDIO");
+        //HACER ALGO ACA
+        textoPuntaje.text = puntos + " puntos";
+        gameOverData.SetActive(true);
+    }
+}
