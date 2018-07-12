6d77ad571dac8bed38f8eb5be285e5afd3f4ea4e
Nicolas Valdearenas <nicolasvalde@gmail.com>
2018-07-11 21:43:14 -0300

    Agregados bouncers
    Agregados 3 bouncers circulares y 2 prismas
    
    Corregido material para fricción de la bola
    
    El material ahora tiene propiedad bounciness
    
    Se aplicó material al contorno del tablero

---
+++ Pinball/Assets/Scripts/Bumper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour {
+    public float force = 100.0f;
+    public float forceRadius = 1.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (Collider col in Physics.OverlapSphere(transform.position, forceRadius))
+        {
+            if (col.GetComponent<Rigidbody>())
+            {
+                col.GetComponent<Rigidbody>().AddExplosionForce(force, transform.position, forceRadius);
+            }
+        }
+    }
+}
