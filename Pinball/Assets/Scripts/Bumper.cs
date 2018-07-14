7925d426d5cc276f70817833f4b17daf0baf15dc
Nicolas Valdearenas <nicolasvalde@gmail.com>
2018-07-14 18:32:40 -0300

    Agregadas luces a los bumpers

--- Pinball/Assets/Scripts/Bumper.cs
+++ Pinball/Assets/Scripts/Bumper.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class Bumper : MonoBehaviour {
     public float force = 100.0f;
     public float forceRadius = 1.0f;
-    public bool encendido;
     public int puntos = 0;
+    //Se toma el renderer del objeto para trabajar sobre el material e iluminación
+    private MeshRenderer renderer;
     private GameController gameController;
     // Use this for initialization
     void Start () {
+
+        renderer = GetComponent<MeshRenderer>();
+
+        renderer.material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+
         //Se hace uso de la instancia del GameController ya existente, tanto en los bumpers como en la bola
-		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
         if (gameControllerObject != null)
         {
             gameController = gameControllerObject.GetComponent<GameController>();
@@ -24,8 +30,8 @@ public class Bumper : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -34,8 +40,25 @@ public class Bumper : MonoBehaviour {
             if (col.GetComponent<Rigidbody>())
             {
                 col.GetComponent<Rigidbody>().AddExplosionForce(force, transform.position, forceRadius);
+                encender();
             }
         }
         gameController.sumarPuntos(puntos);
     }
+
+    private void encender()
+    {
+        renderer.material.EnableKeyword("_EMISSION");
+        renderer.UpdateGIMaterials();
+        //La espera se tiene que hacer en una co-rutina de tipo IEnumerator para que funcione
+        StartCoroutine(Pausa());  
+    }
+
+    IEnumerator Pausa()
+    {
+        yield return new WaitForSecondsRealtime(0.2f);
+        renderer.material.DisableKeyword("_EMISSION");
+        renderer.UpdateGIMaterials();
+    }
+
 }
